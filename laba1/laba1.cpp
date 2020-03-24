d7d0dbf0e36d2877297428f89045ecff843aeb31
XOKAGE95 <56272442+XOKAGE95@users.noreply.github.com>
2020-03-24 23:02:14 +0400

    Add files via upload

--- laba1/laba1.cpp
+++ laba1/laba1.cpp
@@ -9,58 +9,58 @@
 #include <glut.h>
 #define PI 3.1415926535
 
-float a = -5;
+//Границы отрезка
+float a = -5; 
 float b = 10;
-float N = 200;
+
+float N = 400;
 double h;
 
 using namespace std;
 
 ofstream file;
-
-float f(float x)
+// Функция для нахождения 1 и 2 производной
+double f(double x)
 {
     return ((4+3*x*x*x)/(x*pow((2 + x*x*x), 2/3)));
-
+    //return (2 * sqrt((1 - sqrt(x)) / (1 + sqrt(x))));
 }
 
-float f1(float x)
+//Функция и ее производная для нахождения корня
+double f1(float x)
 {
     return (x*x*x*x*x*x + 101*x*x*x*x*x + 425*x*x*x*x - 425*x*x - 101*x - 1);
 }
 
+double f2(float x)
+{
+    return (6*x*x*x*x*x + 505*x*x*x*x + 1700*x*x*x-850*x-101);
+}
+
+//Отрисовка координатной системы
 void CoordinateSystem()
 {
 
     glLineWidth(1.0);       //Òîëùèíà ëèíèé
     glColor3d(0, 0, 0);     //Öâåò ëèíèé - ÷åðíûé
     glBegin(GL_LINES);
-    glVertex3d(-1.4, 0.0, -5.0);
-    glVertex3d(1.4, 0.0, -5.0);
-    glVertex3d(1.4, 0.0, -5.0);
-    glVertex3d(1.2, -0.1, -5.0);
-    glVertex3d(1.2, 0.1, -5.0);
-    glVertex3d(1.4, 0.0, -5.0);
-    glVertex3d(0.0, -1.4, -5.0);
-    glVertex3d(0.0, 1.4, -5.0);
-    glVertex3d(0.0, 1.4, -5.0);
-    glVertex3d(0.1, 1.2, -5.0);
-    glVertex3d(-0.1, 1.2, -5.0);
-    glVertex3d(0.0, 1.4, -5.0);
+    glVertex2d(-100, 0);
+    glVertex2d(100, 0);
+    glVertex2d(0, -100);
+    glVertex2d(0, 100);
     glEnd();
 
 }
 
+//Функция для обработки изменения окна
 void ChangeSize1(int w, int h)
 {
     if(h == 0)
         h = 1;
-    float ratio = 1.0* w / h;
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glViewport(0, 0, w, h);
-    gluPerspective(35,ratio,3,1000);
-    gluOrtho2D(0, 0, 0, 0);
+    glOrtho(-50.0, 50.0, -50.0, 50.0, -100.0, 100.0);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 }
@@ -69,16 +69,15 @@ void ChangeSize2(int w, int h)
 {
     if(h == 0)
         h = 1;
-    float ratio = 1.0* w / h;
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glViewport(0, 0, w, h);
-    gluPerspective(35,ratio,3,1000);
-    gluOrtho2D(0, 0, 0, 0);
+    glOrtho(-20.0, 20.0, -20.0, 20.0, -100.0, 100.0);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 }
 
+//1 задание. Выводит 1 производную 1 порядка точности и 1 производную 2 порядка точности для сравнения
 void task1()
 {
     file.open("Task1.txt");
@@ -86,66 +85,83 @@ void task1()
     glClear(GL_COLOR_BUFFER_BIT);
     CoordinateSystem();
     glLoadIdentity();
-    glColor3d(0.3, 0.5, 1.0);
-    float x = a;
-    float y, y1;
+    glColor3ub(0,0,255);
+    double x = a;
+    double y, y1;
     y1 = (f(x+h) - f(x))/h;
+    glLineWidth(2);
+    // 1 производная 1 порядка точности
+    glBegin(GL_LINES);
+    while (x <= b)
+    {
+        if ((x > -0.2) & (x < 0.2)) x = 0.2;
+        y = y1;
+        glVertex3d(x, y, 0.0);
+        y1 = (f(x+h) - f(x))/h;
+        glVertex3d(x,y1, 0.0);
+        file << x << "\t" << y1 << endl;
+        x+= h;
+    }
+    glEnd();
+    file.close();
+    //1 производная 2 порядка точности
+    file.open("Task2.txt");
+    glColor3ub(255,0,0);
+    x = a;
+    y1 = (f(x+h) - f(x-h))/2/h;
+    glLineWidth(1);
     glBegin(GL_LINES);
     while (x <= b)
     {
         if ((x > -0.2) & (x < 0.2)) x = 0.2;
         if (x == pow(-2, 1/3)) continue;
         y = y1;
-        glVertex3d(x/9.75002, y/19.91, -5.0);
-        y1 = (f(x+h) - f(x))/h;
-        glVertex3d(x/9.75002, y1/19.91, -5.0);
+        glVertex2d(x, y);
+        y1 = (f(x+h) - f(x-h))/2/h;
+        glVertex2d(x, y1);
         file << x << "\t" << y1 << endl;
         x+= h;
     }
     glEnd();
-    file.close();
+
     glutSwapBuffers();
 }
-
-void task2()
+	
+void task2()	// Отрисовка 1 производной 2 порядка точности
 {
-    file.open("Task2.txt");
     glClearColor(1.0, 1.0, 1.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
     CoordinateSystem();
     glLoadIdentity();
-    glColor3d(0.3, 0.5, 1.0);
-    float x = a;
-    float y, y1;
+    glColor3ub(255,0,0);
+    double x = a;
+    double y, y1;
     y1 = (f(x+h) - f(x-h))/2/h;
     glBegin(GL_LINES);
-    while (x <= b)
     while (x <= b)
     {
         if ((x > -0.2) & (x < 0.2)) x = 0.2;
         if (x == pow(-2, 1/3)) continue;
         y = y1;
-        glVertex3d(x/9.75002, y/19.91, -5.0);
+        glVertex2d(x, y);
         y1 = (f(x+h) - f(x-h))/2/h;
-        glVertex3d(x/9.75002, y1/19.91, -5.0);
-        file << x << "\t" << y1 << endl;
+        glVertex2d(x, y1);
         x+= h;
     }
     glEnd();
-    file.close();
     glutSwapBuffers();
 }
-
-void task3()
+	
+void task3() // Отрисовка 2 произовдной 2 порядка точности
 {
     file.open("Task3.txt");
     glClearColor(1.0, 1.0, 1.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
     CoordinateSystem();
     glLoadIdentity();
-    glColor3d(0.3, 0.5, 1.0);
-    float x = a;
-    float y, y1;
+    glColor3ub(0, 255, 0);
+    double x = a;
+    double y, y1;
     y1 = (f(x+h) - 2*f(x)+f(x-h))/(h*h);
     glBegin(GL_LINES);
     while (x <= b)
@@ -153,10 +169,9 @@ void task3()
         if ((x > -0.2) & (x < 0.2)) x = 0.2;
         if (x == pow(-2, 1/3)) continue;
         y = y1;
-        glVertex3d(x/10, y/20, -5.0);
-        y1 = (f(x+h) - 2*f(x) + f(x-h))/(h*h);
-        //cout << x << "\tF1 = " << f(x+h) << "\tF2 = " << f(x) << "\tF3 = " << f(x-h) << endl;
-        glVertex3d(x/10, y1/20, -5.0);
+        glVertex2d(x, y);
+        y1 = (f(x+h) - 2*f(x) + f(x-h))/h/h;
+        glVertex2d(x, y1);
         file << x << "\t" << y1 << endl;
         x+= h;
     }
@@ -193,27 +208,33 @@ bool Dihotomia()
     return true;
 }
 
-bool Nuton()
+bool Nuton()	//Метод Ньютона
 {
     cout << "Enter Xn: ";
-    float Xn;
+    double Xn;
     cin >> Xn;
-    float Xn1 = Xn;
     cout << "Enter eps: ";
-    float eps;
+    double eps;
     cin >> eps;
+    int count;
+    double y;
     do
     {
-        Xn1 = Xn - f1(Xn)/((f1(Xn+h) - f(Xn-h))/2/h);
+        if (count > 10000000) return false;
+        y = f1(Xn)/f2(Xn);
+        Xn = Xn - y;
+        count ++;
     }
-    while(abs(Xn1 - Xn) < eps);
-    cout << "X = " << Xn1 << endl;
+    while((abs(y) >= eps) && (count < 1000000000));
+    cout << "X = " << Xn << endl;
+    return true;
 }
+
 int main (int argc, char **argv)
 {
     h = (b-a)/N;
-    if(!Dihotomia()) cout << "Error" << endl;
-    Nuton();
+    //if(!Dihotomia()) cout << "Error" << endl;
+    if (!Nuton()) cout << "Error" << endl;
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(860, 620);
@@ -221,20 +242,18 @@ int main (int argc, char **argv)
     glutCreateWindow("task1");
     glutDisplayFunc(task1);
     glutReshapeFunc(ChangeSize1);
-    //glutIdleFunc(task1);
     // задание 2
     glutInitWindowSize(860, 620);
     glutInitWindowPosition(20, 20);
     glutCreateWindow("task2");
     glutDisplayFunc(task2);
     glutReshapeFunc(ChangeSize1);
-    //glutIdleFunc(task2);
-        glutInitWindowSize(860, 620);
+    //3 задание
+    glutInitWindowSize(860, 620);
     glutInitWindowPosition(20, 20);
     glutCreateWindow("task3");
     glutDisplayFunc(task3);
     glutReshapeFunc(ChangeSize2);
-    //glutIdleFunc(task2);
     glutMainLoop();
     return 0;
 }
